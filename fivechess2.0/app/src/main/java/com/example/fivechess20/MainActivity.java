fd73cd71c1f3bf7558041fd430ab42a0ed50a6d8
wisdombyzf <781516223@qq.com>
2017-05-02 12:40:43 +0800

    fivechess

---
+++ fivechess2.0/app/src/main/java/com/example/fivechess20/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.fivechess20;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /*public void c()
+    {
+        Intent intent=new Intent();
+        intent.setClass(MainActivity.this,wuziq.class);
+        startActivity(intent);
+
+    }*/
+
+}
